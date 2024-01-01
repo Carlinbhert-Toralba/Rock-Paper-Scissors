dee6f3a38ab5fb8679c66d53c2f175d49d98f52d
carl <cadg.toralba@gmail.com>
2024-01-02 01:23:45 +0800

    removed pseudo code

--- javascript.js
+++ javascript.js
@@ -1,22 +1,3 @@
-// function randomly returns "Rock", "Paper" or "Scissors"
-// create function
-// create array of 3 choices named 'choice'
-// create variable 'pick' and do math.random for computer 'choice'
-// return pick
-
-// function plays a single round
-// create function with parameters playerSelection and computerSelection
-// if both choices are the same recall the function
-// if player wins increase player score
-// if computer wins increase computer score
-
-// function plays 5 rounds
-// create function game
-// add a for loop for 5 rounds
-// create variable for playerSelection with prompt
-// create variable for computerSelection calling first function
-// have the round played
-// create game score conditional
 "use strict";
 
 let playerScore = 0;
