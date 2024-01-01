bf57ebcad15fe46c9d13d2620d289fb4862aa504
carl <cadg.toralba@gmail.com>
2024-01-01 23:53:01 +0800

    added a function for playing a single round

--- javascript.js
+++ javascript.js
@@ -24,4 +24,20 @@ let computerScore = 0;
 function getComputerChoice() {
   const choice = ["rock", "paper", "scissors"];
   const pick = choice[Math.floor(Math.random() * choice.length)];
+
+  return pick;
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    playRound();
+  } else if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissor" && computerSelection == "paper")
+  ) {
+    playerScore++;
+  } else {
+    computerScore++;
+  }
 }
