a2b0c38217df5ded6048ecaed7bf3cc0f8654797
carl <cadg.toralba@gmail.com>
2024-01-02 01:29:50 +0800

    still in process of troubleshooting game function, removed unnecessary pieces of code in said function

--- javascript.js
+++ javascript.js
@@ -15,36 +15,30 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "It's a tie!";
+    playRound();
   } else if (
     (playerSelection === "rock" && computerSelection == "paper") ||
     (playerSelection === "paper" && computerSelection == "rock") ||
     (playerSelection === "scissor" && computerSelection == "paper")
   ) {
     playerScore++;
-    return "Player wins!";
   } else {
     computerScore++;
-    return "Computer wins!";
   }
 }
-console.log(playRound(computerSelection, playerSelection));
-// function game() {
-//   for (let i = 0; i < 5; i++) {
-//     const playerSelection = prompt(
-//       "Choose rock, paper or scissors".toLowerCase()
-//     );
 
-//     const computerChoice = getComputerChoice();
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound();
+  }
 
-//     const result = playRound(playerSelection, computerChoice);
+  if (playerScore > computerScore) {
+    return "Player wins!";
+  } else if (playerScore < computerScore) {
+    return "Computer wins!";
+  } else {
+    return "It's a tie";
+  }
+}
 
-//     if (playerScore > computerScore) {
-//       return "Player wins!";
-//     } else if (playerScore < computerScore) {
-//       return "Computer wins!";
-//     } else {
-//       return "It's a tie";
-//     }
-//   }
-// }
+game();
