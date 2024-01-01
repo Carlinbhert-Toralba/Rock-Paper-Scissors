5172c7e6ad8c71ee252d66fbddae9eba943b6830
carl <cadg.toralba@gmail.com>
2024-01-02 01:22:42 +0800

    added variables for computer and player selections, troubleshooted game function

--- javascript.js
+++ javascript.js
@@ -17,9 +17,13 @@
 // create variable for computerSelection calling first function
 // have the round played
 // create game score conditional
+"use strict";
 
 let playerScore = 0;
 let computerScore = 0;
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Choose rock, paper or scissors".toLowerCase());
 
 function getComputerChoice() {
   const choice = ["rock", "paper", "scissors"];
@@ -30,14 +34,36 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    playRound();
+    return "It's a tie!";
   } else if (
-    (playerSelection == "rock" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "scissor" && computerSelection == "paper")
+    (playerSelection === "rock" && computerSelection == "paper") ||
+    (playerSelection === "paper" && computerSelection == "rock") ||
+    (playerSelection === "scissor" && computerSelection == "paper")
   ) {
     playerScore++;
+    return "Player wins!";
   } else {
     computerScore++;
+    return "Computer wins!";
   }
 }
+console.log(playRound(computerSelection, playerSelection));
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     const playerSelection = prompt(
+//       "Choose rock, paper or scissors".toLowerCase()
+//     );
+
+//     const computerChoice = getComputerChoice();
+
+//     const result = playRound(playerSelection, computerChoice);
+
+//     if (playerScore > computerScore) {
+//       return "Player wins!";
+//     } else if (playerScore < computerScore) {
+//       return "Computer wins!";
+//     } else {
+//       return "It's a tie";
+//     }
+//   }
+// }
